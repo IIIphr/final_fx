114760cb2abf791771f6750321ff5da3b426894b
sepehr.ebi.1380@gmail.com <sepehr.ebi.1380@gmail.com>
2020-07-19 15:08:56 +0430

    added the ability to set the port of server socket when started ( in System.in )

--- src/Server/Model/Main.java
+++ src/Server/Model/Main.java
@@ -2,9 +2,16 @@ package Server.Model;
 
 import Server.Model.Communications.Server;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Thread server=new Server();
+        Scanner scanner=new Scanner(System.in);
+        System.out.printf("port:");
+        int port=scanner.nextInt();
+        ((Server)server).setPort(port);
+        ((Server)server).start_socket();
         server.start();
     }
 }
